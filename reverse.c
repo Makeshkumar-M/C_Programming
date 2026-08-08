40c14cc829fb76a1bf6dda0787075a2915401675
Makeshkumar-M <makeshkumarm18@gmail.com>
2026-08-08 18:24:37 +0530

    Reverse the number

---
+++ reverse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int num,rem,rev=0;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    while(num!=0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;    }
+    
+    printf("The reversed number:%d",rev);
+    return 0;
+}
